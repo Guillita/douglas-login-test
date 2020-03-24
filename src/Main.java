b6ffe88488ad1d1dbc825d1a757b55085b0ba886
Guillita <guillermina.claros@gmail.com>
2020-03-24 11:11:23 +0100

    Removed unused imports from Main

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,5 @@
-import java.io.IOException;
-
 import User.IUser;
 import User.User;
-import components.CaptchaForm.CaptchaForm;
-import components.CaptchaForm.ICaptchaForm;
-import components.HomePage.HomePage;
-import components.HomePage.IHomePage;
-import components.UserPage.IUserPage;
-import components.UserPage.UserPage;
-import driversConfiguration.Browsers;
-import net.sourceforge.tess4j.TesseractException;
-import tests.login.ResetCredentials;
-import tests.login.Success;
-import tests.login.WrongCredentials;
 import testsSuites.TestSuiteLogin;
 
 public class Main {
