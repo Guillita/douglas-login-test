9b7df63445d604dda97f76e451964f45d35a0f9b
Guillita <guillermina.claros@gmail.com>
2020-03-24 14:53:13 +0100

    Updated UserPage

--- src/components/UserPage/IUserPage.java
+++ src/components/UserPage/IUserPage.java
@@ -1,6 +1,9 @@
-package components.UserPage;
+package components.userpage;
+
+import org.openqa.selenium.WebDriver;
 
 public interface IUserPage {
-	public String getClientNumberMessageLocator();
-	public String getClientNumberMessage();
+	public String getClientNumberTextLocator();
+	public String getClientNumberText();
+    public String findTextByTag(WebDriver webDriver, String tag);
 }
--- src/components/UserPage/UserPage.java
+++ src/components/UserPage/UserPage.java
@@ -1,16 +1,23 @@
-package components.UserPage;
+package components.userpage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
 public class UserPage implements IUserPage {
 	
 	private static final String clientNumberMessage = "Ihre Kundennummer bei Rückfragen:";
 	private static final String clientNumberMessageLocator = "//span[contains(text(),'Ihre Kundennummer bei Rückfragen:')]";
 
-	public String getClientNumberMessageLocator() {
+	public String getClientNumberTextLocator() {
 		return clientNumberMessageLocator;
 	}
 	
-	public String getClientNumberMessage() {
+	public String getClientNumberText() {
 		return clientNumberMessage;
+	}
+
+	public String findTextByTag(WebDriver webDriver, String tag) {
+		return webDriver.findElement(By.tagName(tag)).getText();
 	}
 
 }
