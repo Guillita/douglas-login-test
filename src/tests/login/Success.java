a26bedaa49af3cd58d52c2017829b9f587695125
Guillita <guillermina.claros@gmail.com>
2020-03-24 14:26:42 +0100

    Refactored Success test

--- src/tests/login/Success.java
+++ src/tests/login/Success.java
@@ -12,34 +12,49 @@ import User.IUser;
 import components.HomePage.IHomePage;
 import components.UserPage.IUserPage;
 
+/* This class test the following requirement:
+ * 
+ *  - As an user with correct credentials I would like to be able to login to the webshop
+ */
 public class Success {
 	
     WebDriver webDriver = null;
   
-	// As an user with correct credentials I would like to be able to login to the webshop    
 	@ParameterizedTest
 	public void testLoginSuccess(WebDriver webDriver, IHomePage homePage, IUser user, IUserPage userPage) {
         WebDriverWait wait = new WebDriverWait(webDriver,30);
         wait.until(ExpectedConditions.elementToBeClickable(By.name(homePage.getEmailInputName())));
-        WebElement email = homePage.findElementByName(webDriver, homePage.getEmailInputName());
-		email.click();
-        email.sendKeys(user.getEmail());
         
-        WebElement password = (webDriver).findElement(By.name((homePage.getPasswordInputName())));
-		password.click();
-        password.sendKeys(user.getPassword());
-        
-        WebElement loginButton = (webDriver).findElement(By.name(homePage.getLoginSubmitButtonName()));
-        loginButton.submit();
-        
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(userPage.getClientNumberMessageLocator())));
-        String bodyText = webDriver.findElement(By.tagName("body")).getText();
-        
-        Assert.assertTrue("Text not found!", bodyText.contains(userPage.getClientNumberMessage()));
+        setUserEmailInHomePage(webDriver, homePage, user);
+        setUserPasswordInHomePage(webDriver, homePage, user);
+        clickOnLoginSubmitButtonFromHomePage(webDriver, homePage);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(userPage.getClientNumberTextLocator())));
+        checkClientNumberTextPresence(webDriver, userPage);
                 
         webDriver.quit();
 	}
 	
+	public void setUserEmailInHomePage(WebDriver webDriver, IHomePage homePage, IUser user) {
+        WebElement email = homePage.findElementByName(webDriver, homePage.getEmailInputName());
+		email.click();
+        email.sendKeys(user.getEmail());
+	}
+	
+	public void setUserPasswordInHomePage(WebDriver webDriver, IHomePage homePage, IUser user) {
+        WebElement password = homePage.findElementByName(webDriver, homePage.getPasswordInputName());
+		password.click();
+        password.sendKeys(user.getWrongPassword());
+	}
+
+    public void clickOnLoginSubmitButtonFromHomePage(WebDriver webDriver, IHomePage homePage) {
+    	WebElement loginButton = homePage.findElementByName(webDriver, homePage.getLoginSubmitButtonName());
+        loginButton.submit();
+    }
+    
+    public void checkClientNumberTextPresence(WebDriver webDriver, IUserPage userPage) {
+    	String bodyText = userPage.findTextByTag(webDriver, "body");
+        Assert.assertTrue("Text not found!", bodyText.contains(userPage.getClientNumberText()));
+    }
 
 
 }
